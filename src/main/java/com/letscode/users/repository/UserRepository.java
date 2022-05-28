c6d3c9f0d93029b72c8b1de81f87d3a439fcbd60
nori-marcos <marcos.noriyuki.miyata@gmail.com>
2022-05-28 19:13:24 -0300

    feat:Criacao do Repository

--- src/main/java/com/letscode/users/repository/UserRepository.java
+++ src/main/java/com/letscode/users/repository/UserRepository.java
@@ -1,4 +1,11 @@
 package com.letscode.users.repository;
 
-public class UserRepository {
+import com.letscode.users.model.User;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, Long> {
+
+    User findByUuid(String uuid);
 }
