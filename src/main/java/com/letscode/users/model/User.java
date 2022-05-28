3852f6fd75a24f6bca3658ba217e34cff27f8f09
nori-marcos <marcos.noriyuki.miyata@gmail.com>
2022-05-28 19:11:59 -0300

    feat: Implementacao do model User

--- src/main/java/com/letscode/users/model/User.java
+++ src/main/java/com/letscode/users/model/User.java
@@ -1,5 +1,27 @@
 package com.letscode.users.model;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Document
+@NoArgsConstructor
+@Data
 public class User {
-    
+
+    @Id
+    private Long id;
+
+    private String name;
+
+    private String cpf;
+
+    private String password;
+
+    private String email;
+
+    private String uuid = UUID.randomUUID().toString();
 }
